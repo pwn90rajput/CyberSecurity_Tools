1a802210d6f1af6a72c25e2e90e1039a34f481b7
Pawan Kumar <73460364+pwn90rajput@users.noreply.github.com>
2025-08-03 16:37:03 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cybersec-tools/'
+  base: '/CyberSecurity_Tools/' // GitHub repo name as base path
 })
